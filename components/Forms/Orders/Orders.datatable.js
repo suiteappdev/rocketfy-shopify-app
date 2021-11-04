275cac2800d6532399deebc0767c14c223b067d9
andrew <listerine1989@gmail.com>
2021-11-04 15:04:19 -0500

    se verifica error react

--- components/Forms/Orders/Orders.datatable.js
+++ components/Forms/Orders/Orders.datatable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useIndexResourceState, Card, IndexTable} from '@shopify/polaris';
 import moment from 'moment';
-import { getCourrier } from '../../../helpers/location.helper';
 
 const Datatable = (props)=>{
       const resourceName = {
@@ -11,32 +10,25 @@ const Datatable = (props)=>{
     
       const {selectedResources, allResourcesSelected, handleSelectionChange} =
       useIndexResourceState(props.orders);
-
+    
       const rowMarkup = props.orders.map(
-        ({node}, index) => {
-          if(props.cities){
-            /*let courrier = await getCourrier(props.cities, 'morroa');
-            console.log("courrier", courrier);*/
-          }
-
-          return(
-              <IndexTable.Row
-                id={index}
-                key={index}
-                selected={selectedResources.includes(index)}
-                position={index}
-              >
-              <IndexTable.Cell>{node.name}</IndexTable.Cell>
-              <IndexTable.Cell>{moment(node.createdAt).format('LLL')}</IndexTable.Cell>
-              <IndexTable.Cell>{`${node.customer.firstName} ${node.customer.lastName}`}</IndexTable.Cell>
-              <IndexTable.Cell>{`${node.billingAddress.address1} ${node.billingAddress.address2}`}</IndexTable.Cell>
-              <IndexTable.Cell>{node.shippingAddress.city}</IndexTable.Cell>
-              <IndexTable.Cell>{node.shippingAddress.province}</IndexTable.Cell>
-              <IndexTable.Cell>{'Interrapidisimo'}</IndexTable.Cell>
-              <IndexTable.Cell>${node.currentTotalPriceSet.shopMoney.amount}</IndexTable.Cell>
-              </IndexTable.Row>            
-          );
-        }
+        async ({node}, index) => (
+          <IndexTable.Row
+            id={index}
+            key={index}
+            selected={selectedResources.includes(index)}
+            position={index}
+          >
+          <IndexTable.Cell>{node.name}</IndexTable.Cell>
+          <IndexTable.Cell>{moment(node.createdAt).format('LLL')}</IndexTable.Cell>
+          <IndexTable.Cell>{`${node.customer.firstName} ${node.customer.lastName}`}</IndexTable.Cell>
+          <IndexTable.Cell>{`${node.billingAddress.address1} ${node.billingAddress.address2}`}</IndexTable.Cell>
+          <IndexTable.Cell>{node.shippingAddress.city}</IndexTable.Cell>
+          <IndexTable.Cell>{node.shippingAddress.province}</IndexTable.Cell>
+          <IndexTable.Cell>{'Interrapidisimo'}</IndexTable.Cell>
+          <IndexTable.Cell>${node.currentTotalPriceSet.shopMoney.amount}</IndexTable.Cell>
+          </IndexTable.Row>
+        ),
       );
     
       return (
