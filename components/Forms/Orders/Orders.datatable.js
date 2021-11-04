78a6d301c9197352c1d4a0532cfb38efec0e4a09
andrew <listerine1989@gmail.com>
2021-11-04 17:34:26 -0500

    get courrier test

--- components/Forms/Orders/Orders.datatable.js
+++ components/Forms/Orders/Orders.datatable.js
@@ -15,11 +15,8 @@ const Datatable = (props)=>{
       const rowMarkup = props.orders.map(
          ({node}, index) => {
 
-          if(props.cities && props.cities.length){
-            node.courrier = getCourrier(props.cities, node.shippingAddress.city);
-            console.log("node.courrier", node); 
-          }
-           
+          let courrier = getCourrier(props.cities, node.shippingAddress.city);
+           console.log("node.courrier", courrier); 
            return(
             <IndexTable.Row
               id={index}
