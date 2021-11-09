83bbf837315b1e6940cecd8b199e74f4330f8d53
andrew <listerine1989@gmail.com>
2021-11-09 12:06:44 -0500

    deploy

--- server/server.js
+++ server/server.js
@@ -46,8 +46,7 @@ app.prepare().then(async () => {
   apiRoutes.post('/api/webhook-notification', async (ctx)=>{
       console.log("BODY", ctx.request.body);
       ctx.response.status = 200;
-      ctx.request.body
-      await Shopify.Webhooks.Registry.process(ctx.req, ctx.res);
+      ctx.request.body = ctx.request.body;
       console.log(`Webhook processed, returned status code 200`);
   });
 
