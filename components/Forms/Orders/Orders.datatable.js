1e6eac7d5c805c7a66d03fa5039ef0037586b9c2
andrew <listerine1989@gmail.com>
2021-11-17 11:16:56 -0500

    deploy

--- components/Forms/Orders/Orders.datatable.js
+++ components/Forms/Orders/Orders.datatable.js
@@ -52,8 +52,8 @@ const Datatable = (props)=>{
                   let response = await callQuery({ id : o}).catch((e)=>console.log(e.message));
                   if(response){
                     console.log("response", response);
-                    let order = await createOrder(response.data.order);
-                    console.log("o", order)
+                    //let order = await createOrder(response.data.order);
+                    //console.log("o", order)
                   }
             }
           },
