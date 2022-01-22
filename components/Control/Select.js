6f8e954ff2f299f0c8b665ea500b4b77c8232646
andrew <listerine1989@gmail.com>
2022-01-22 11:49:47 -0500

    test response

--- components/Control/Select.js
+++ components/Control/Select.js
@@ -16,6 +16,7 @@ const City  = (props)=>{
             };
 
             let response  = await getCities().catch(()=>console.log("No se pudieron recuperar las ciudades"));
+            console.log(response);
             setCities(mapOptions(response));
         }
 
--- helpers/location.helper.js
+++ helpers/location.helper.js
@@ -48,9 +48,7 @@ const mapCourrier = (orders, cities)=>{
 const getCities = (cod)=>{
     return new Promise(async (resolve, reject)=>{
         let response = await PostRequest(`${process.env.ROCKETFY_APIHOST}/api/public/cities`, { cod : cod }).catch((e)=>reject(e));
-        console.log("RESPONSE", response);
-       
-        resolve(response.data.data);
+        resolve(response.data);
     });
 }
 
