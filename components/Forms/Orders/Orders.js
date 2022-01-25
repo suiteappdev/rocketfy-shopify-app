5e0d1bdf78fbab41ba33609e087ab12162249bba
andrew <listerine1989@gmail.com>
2022-01-25 10:20:58 -0500

    added recource list

--- components/Forms/Orders/Orders.js
+++ components/Forms/Orders/Orders.js
@@ -1,11 +1,12 @@
 import React, {useCallback, useRef, useState} from 'react';
-import {useIndexResourceState,Banner,Spinner, Card, IndexTable, Button, Modal, Stack, TextContainer, TextField, FormLayout} from '@shopify/polaris';
+import {Avatar, ResourceItem, ResourceList, TextStyle, useIndexResourceState,Banner,Spinner, Card, IndexTable, Button, Modal, Stack, TextContainer, TextField, FormLayout} from '@shopify/polaris';
 import moment from 'moment'; 
 import { mapCourrier } from '../../../helpers/location.helper';
 import { ORDER_BY_ID } from '../../../graphql/querys/orderById.query';
 import { createOrder, shippingCost } from '../../../helpers/order.helper';
 import { useQuery } from '@apollo/client';
 import City from '../../Control/Select';
+import './Orders.module.css';
 
 const Datatable = (props)=>{
      const [orders, setOrders]  = useState([]);
@@ -250,6 +251,41 @@ const Datatable = (props)=>{
                 <div style={{width:'50%'}}>
                   {loading  ? ( <Spinner accessibilityLabel="Spinner example" size="large" />) : (
                     <div>
+                      <Card>
+                        <ResourceList
+                          resourceName={{singular: 'customer', plural: 'customers'}}
+                          items={[
+                            {
+                              id: 145,
+                              url: 'customers/145',
+                              avatarSource:
+                                'https://burst.shopifycdn.com/photos/freelance-designer-working-on-laptop.jpg?width=746',
+                              name: 'Yi So-Yeon',
+                              location: 'Gwangju, South Korea',
+                            },
+                          ]}
+                          renderItem={(item) => {
+                            const {id, url, avatarSource, name, location} = item;
+
+                            return (
+                              <ResourceItem
+                                id={id}
+                                url={url}
+                                media={
+                                  <Avatar customer size="medium" name={name} source={avatarSource} />
+                                }
+                                accessibilityLabel={`View details for ${name}`}
+                                name={name}
+                              >
+                                <h3>
+                                  <TextStyle variation="strong">{name}</TextStyle>
+                                </h3>
+                                <div>{location}</div>
+                              </ResourceItem>
+                            );
+                          }}
+                        />
+                      </Card>
                       <p style={{textAlign:'center'}}>Resultado de cotización</p>
                       {mapCurrier(currentOrder)}
                     </div>
