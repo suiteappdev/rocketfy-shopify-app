85e133523d014428e1a97c36d92d772ccd08c5cc
andrew <listerine1989@gmail.com>
2022-01-28 11:31:54 -0500

    added loader with color

--- components/Forms/Orders/Orders.js
+++ components/Forms/Orders/Orders.js
@@ -278,7 +278,20 @@ const Datatable = (props)=>{
                     <Button primary onClick={()=>getShipping()}>Cotizar</Button>
                 </div>
                 <div style={{width:'50%', paddingLeft:'10px', paddingRight:'10px', display:'flex', alignItems : 'center', justifyContent:'center', flexDirection:'column', marginTop:'60px'}}>
-                  {loading  ? (<img src="svg-loaders/puff.svg" width="50" alt="" />) : (
+                  {loading  ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" stroke="#7a13c1" style="&#10;    /* background-color: green; */&#10;">
+                    <g fill="none" fill-rule="evenodd" stroke-width="2">
+                        <circle cx="22" cy="22" r="1">
+                            <animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
+                            <animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
+                        </circle>
+                        <circle cx="22" cy="22" r="1">
+                            <animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
+                            <animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
+                        </circle>
+                    </g>
+                  </svg>
+                  ) : (
                     <div style={{width:'100%'}}>
                       {curriers.length > 0 ? ( <p style={{textAlign:'center', marginBottom:'10px'}}>Resultado de cotización</p>) : (null)}
                       <Card>
