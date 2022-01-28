d93363b35c6704c59aa824030b5f4e9d468bc7cc
andrew <listerine1989@gmail.com>
2022-01-28 18:26:30 -0500

    fix current order title

--- components/Forms/Orders/Orders.js
+++ components/Forms/Orders/Orders.js
@@ -271,7 +271,7 @@ const Datatable = (props)=>{
                     }} name={'from'}></City>
                     <br />
                     <p style={{ marginBottom:'10px'}}>
-                    Destino del paquete. - {currentOrder.shippingAddress.city || ''}
+                    Destino del paquete. - {currentOrder ? currentOrder.shippingAddress.city : ''}
                     </p>
                     <City placeholder="Ciudad destino" onChange={(value)=>{
                       setshipping({...shipping, to : value});
