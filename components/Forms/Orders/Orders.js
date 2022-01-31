34bdfc7de09704b324b0defeeb352c1f580ce9dd
andrew <listerine1989@gmail.com>
2022-01-31 16:43:07 -0500

    enable success message

--- components/Forms/Orders/Orders.js
+++ components/Forms/Orders/Orders.js
@@ -205,7 +205,7 @@ const Datatable = (props)=>{
               
               if(response){
                 let order = await createOrder(response.data, shipping).catch((e)=>console.log(e.message));
-                if(order && order.orderData){
+                if(order){
                     setOrderSuccess(true);
                     toggleModal();
                 }
