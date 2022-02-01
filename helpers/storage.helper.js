59e548c053a1e3e4afb471759973c4f28c29b323
andrew <listerine1989@gmail.com>
2022-01-31 20:06:04 -0500

    fix undefined replace

--- helpers/storage.helper.js
+++ helpers/storage.helper.js
@@ -1,5 +1,7 @@
 const getRocketfyToken = ()=>{
-    return window.localStorage.getItem('rocketfy-token').replace("https://app.rocketfy.co/", "http://localhost:4200/");
+    if(window.localStorage.getItem('rocketfy-token')){
+        return window.localStorage.getItem('rocketfy-token').replace("https://app.rocketfy.co/", "http://localhost:4200/");
+    }
 }
 
 const setRocketfyToken = (value)=>{
