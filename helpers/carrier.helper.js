886c15f1bff0d22da079eba1c3ecd2f448a7e2e3
andrew <listerine1989@gmail.com>
2022-02-03 10:20:10 -0500

    change to fetch

--- helpers/carrier.helper.js
+++ helpers/carrier.helper.js
@@ -1,21 +1,23 @@
-import axios from "axios";
-
 const createCarrier = () =>{
     return new Promise(async (resolve, reject)=>{
         let body ={
-                carrier_service:
-                {
-                    name:"TCC",
-                    callback_url:"http:\/\/tcc.com",
-                    service_discovery:true
-                }
+        carrier_service:
+            {
+                name:"TCC",
+                callback_url:"http:\/\/tcc.com",
+                service_discovery:true
             }
+        }
 
-        let data = await axios.post(`https://rocketfystore.myshopify.com/admin/api/2022-01/carrier_services.json`, body, {
-            "X-Shopify-Access-Token": `asdsa`
-        }).catch((e)=>reject(e));
-       
-        console.log("data", data);
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'X-Shopify-Access-Token': `asdsa`},
+            body : JSON.stringify(body)
+        };
+
+        const response = await fetch(`https://rocketfystore.myshopify.com/admin/api/2022-01/carrier_services.json`, options).catch(e=>reject(e));
+        const data = await response.json().catch(e=>reject(e));
+    
         resolve(data);
     });
 }
