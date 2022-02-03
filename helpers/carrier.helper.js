5fd969f654893d443de422f5a416c209595399ab
andrew <listerine1989@gmail.com>
2022-02-03 15:41:33 -0500

    addded key to body

--- helpers/carrier.helper.js
+++ helpers/carrier.helper.js
@@ -1,13 +1,16 @@
 const createCarrier = (token) =>{
     return new Promise(async (resolve, reject)=>{
-        let body = { name:"TCC", callback_url:"https:\/\/rocketfy-shopify-app.herokuapp.com/api/cotizador", service_discovery:true }
-                const options = {
+            let body = { carrier_service : {
+                name:"TCC", callback_url:"https:\/\/rocketfy-shopify-app.herokuapp.com/api/cotizador", service_discovery:true }
+            }
+            
+            const opts = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
                 body : JSON.stringify(body)
             };
     
-            const response = await fetch(`/create-carrier-service`, options).catch(e=>reject(e));
+            const response = await fetch(`/create-carrier-service`, opts).catch(e=>reject(e));
             const data = await response.json().catch(e=>reject(e));
         
             resolve(data);
