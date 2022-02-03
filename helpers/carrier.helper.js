e49e8608a30a476269323b220075818b86c93230
andrew <listerine1989@gmail.com>
2022-02-03 09:59:26 -0500

    fix error

--- helpers/carrier.helper.js
+++ helpers/carrier.helper.js
@@ -4,17 +4,16 @@ import axios from "axios";
 const createCarrier = () =>{
     return new Promise(async (resolve, reject)=>{
         let body ={
-                carrier_service:
-                {
-                    name:"TCC",
-                    callback_url:"http:\/\/tcc.com",
-                    service_discovery:true
-                }
+            carrier_service: {
+                name:"TCC",
+                callback_url:"http:\/\/tcc.com",
+                service_discovery:true
             }
+        }
 
         let data =  axios.post(`https://rocketfystore.myshopify.com/admin/api/2022-01/carrier_services.json`, body, {
             "X-Shopify-Access-Token": `${''}`
-        }).catch(()=>reject(e));
+        }).catch((e)=>reject(e));
        
         console.log("data", data);
         resolve(data);
