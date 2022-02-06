2289f6ecdbc94cd8414075b6055c85f57c5d591f
andrew <listerine1989@gmail.com>
2022-02-06 11:15:31 -0500

    latest

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.prepare().then(async () => {
   });
 
   apiRoutes.post('/api/cotizador', async (ctx)=>{
-    console.log("body del cotizador", ctx.body);
+    console.log("body del cotizador", ctx);
     ctx.body = { "rates": [ { "service_name": "canadapost-overnight", "service_code": "ON", "total_price": "1295", "description": "This is the fastest option by far", "currency": "CAD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2013-04-12 14:48:45 -0400" }, { "service_name": "fedex-2dayground", "service_code": "2D", "total_price": "2934", "currency": "USD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2013-04-12 14:48:45 -0400" }, { "service_name": "fedex-priorityovernight", "service_code": "1D", "total_price": "3587", "currency": "USD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2013-04-12 14:48:45 -0400" } ] }
     ctx.status = 200;
   });
