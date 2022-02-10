b74f3acd0c1ad7aa491d4d6a7dada64cee12a46a
andrew <listerine1989@gmail.com>
2022-02-10 10:20:24 -0500

    latest

--- helpers/location.helper.js
+++ helpers/location.helper.js
@@ -44,7 +44,9 @@ const mapCourrier = (orders, cities)=>{
 
 const getCities = (cod)=>{
     return new Promise(async (resolve, reject)=>{
-        let response = await PostRequest(`${process.env.ROCKETFY_APIHOST}/api/public/cities`, { cod : cod }).catch((e)=>reject(e));
+        let response = await PostRequest(`${process.env.ROCKETFY_APIHOST}/api/public/cities`, 
+        {query:{"courriers.cod":true},"sort":{"name":1},"limit":0,"skip":0}
+        ).catch((e)=>reject(e));
         resolve(response.data);
     });
 }
