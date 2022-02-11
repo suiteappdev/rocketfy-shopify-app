53d2baa25342f25637702ae873538c6f20083aff
andrew <listerine1989@gmail.com>
2022-02-11 14:59:48 -0500

    latest

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const OrderController  = {
     createOrder : (order)=>{
-        return new Promise((resolve, reject)=>{
+        return new Promise(async (resolve, reject)=>{
             let o = await axios.post(order).catch((e)=>reject(e));
 
             if(o && o.data){
