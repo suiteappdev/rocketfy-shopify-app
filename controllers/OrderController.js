965c79ec19951a60b7f774bee8a317656feaee64
andrew <listerine1989@gmail.com>
2022-02-14 07:57:41 -0500

    latest

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -41,7 +41,7 @@ const OrderController  = {
                             "weight": parseInt(item.grams / 1000)
                         }
                     }),
-                    "carrier" : data.shipping_lines[0].title || 'servientrega'
+                    "carrier" : (data.shipping_lines.length > 0 ?  data.shipping_lines[0].title : 'servientrega')
                 }
 
                 let o = await axios.post(`https://f2b2-190-28-227-176.ngrok.io/api/public/v2/createOrders`, 
