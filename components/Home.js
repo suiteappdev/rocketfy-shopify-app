14b0b90e0600a2180287e12591e8b6ef14552b3c
andrew <listerine1989@gmail.com>
2022-02-21 17:57:36 -0500

    remove image from layout

--- components/Home.js
+++ components/Home.js
@@ -2,10 +2,9 @@ import {  Frame, Page } from "@shopify/polaris";
 import React, { useState, useEffect} from 'react';
 import MainTabs from "./MainTabs";
 import { getAcessToken } from "../helpers/request.helper";
-import { getAppToken, getRocketfyToken, setRocketfyToken } from "../helpers/storage.helper";
-import box2 from '../assets/images/box2.png'
+import { getRocketfyToken, setRocketfyToken } from "../helpers/storage.helper";
 
-const Home = (props) => {
+const Home = () => {
     const [token, setToken] = useState({});
 
     useEffect(() => {
@@ -25,7 +24,6 @@ const Home = (props) => {
 
     return (
         <Page>
-            <img src='https://www.rocketfy.co/static/graf_drop-2821422d99ea86f94f6dc11c038e62b3.png' alt="logo" />;
             {token ? (
                 <Frame>
                     <MainTabs {...token}/>
