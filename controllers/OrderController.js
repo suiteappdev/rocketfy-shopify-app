88414be8040d0eb6efcdba8cd4e27cfbc80c6988
andrew <listerine1989@gmail.com>
2022-03-05 15:55:14 -0500

    test with ngrok

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -63,7 +63,7 @@ const OrderController  = {
                 console.log("rocketfy order", order)
                 //https://api.rocketfy.co/
                 //http://6472-186-43-14-133.ngrok.io
-                let o = await axios.post(`${'http://6472-186-43-14-133.ngrok.io/'}api/public/v2/createOrders`, 
+                let o = await axios.post(`http://4c4e-186-43-14-133.ngrok.io/api/public/v2/createOrders`, 
                         { orders : [order], dbname : auth.customerID}, 
                         { headers : headers }).catch((e)=>console.log(e));
                 
