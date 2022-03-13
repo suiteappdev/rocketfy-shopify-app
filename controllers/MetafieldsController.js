a1bf41e73a96057a918d00e0148c93bd149d8020
andrew <listerine1989@gmail.com>
2022-03-13 13:46:57 -0500

    testing 1 promise

--- controllers/MetafieldsController.js
+++ controllers/MetafieldsController.js
@@ -3,75 +3,33 @@ import { DataType } from "@shopify/shopify-api";
 const MetafieldController  =  {
     size : (RestClient, Graphql)=>{
         let width = async ()=>{
-            return await Graphql.query({ data: `
-                mutation  MetafieldDefinitionCreateMutation  ($input: MetafieldDefinitionInput!) {
-                    metafieldDefinitionCreate(definition: $input) {
-                        userErrors {
-                            field
-                            message
+            return new Promise((resolve, reject)=>{
+                let rs = await Graphql.query({ data: `
+                    mutation  MetafieldDefinitionCreateMutation  ($input: MetafieldDefinitionInput!) {
+                        metafieldDefinitionCreate(definition: $input) {
+                            userErrors {
+                                field
+                                message
+                            }
                         }
                     }
-                }
-            `}, { variables : {
-                    input: {
-                        ownerType: "PRODUCT",
-                        namespace: "dimensiones",
-                        key: "ancho",
-                        type: "number_integer",
-                        validations: [],
-                        name: "ancho",
-                        description: "Defina en (cms) la anchura del paquete"
-                    }
-            }});
-        };
-
-        let height = async ()=>{
-            return await Graphql.query({ data: `
-                mutation  MetafieldDefinitionCreateMutation  ($input: MetafieldDefinitionInput!) {
-                    metafieldDefinitionCreate(definition: $input) {
-                        userErrors {
-                            field
-                            message
+                `}, { variables : {
+                        input: {
+                            ownerType: "PRODUCT",
+                            namespace: "dimensiones",
+                            key: "ancho",
+                            type: "number_integer",
+                            validations: [],
+                            name: "ancho",
+                            description: "Defina en (cms) la anchura del paquete"
                         }
-                    }
-                }
-            `}, { variables : {
-                input: {
-                    ownerType: "PRODUCT",
-                    namespace: "dimensiones",
-                    key: "alto",
-                    type: "number_integer",
-                    validations: [],
-                    name: "alto",
-                    description: "Defina en (cms) la altura del paquete"
-                }
-            }});
+                }}).catch((e)=>reject(e));
+                
+                resolve(rs);
+            });
         };
-
-        let large = async ()=>{
-            return await Graphql.query({ data: `
-                mutation  MetafieldDefinitionCreateMutation  ($input: MetafieldDefinitionInput!) {
-                    metafieldDefinitionCreate(definition: $input) {
-                        userErrors {
-                            field
-                            message
-                        }
-                    }
-                }
-            `}, { variables : {
-                input: {
-                    ownerType: "PRODUCT",
-                    namespace: "dimensiones",
-                    key: "largo",
-                    type: "number_integer",
-                    validations: [],
-                    name: "largo",
-                    description: "Defina en (cms) la longitud del paquete"
-                }
-            }});
-        };
-
-        return Promise.all([width(), height(), large()]);
+      
+        return Promise.all([width]);
     }
  }
 
