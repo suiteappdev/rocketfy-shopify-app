5237c03e756f8fe05605303dbab67700a49b923b
andrew <listerine1989@gmail.com>
2022-03-13 17:57:08 -0500

    added other fields

--- controllers/MetafieldsController.js
+++ controllers/MetafieldsController.js
@@ -1,5 +1,3 @@
-import { DataType } from "@shopify/shopify-api";
-
 const MetafieldController  =  {
     size : (RestClient, Graphql)=>{
         let width = ()=>{
@@ -32,13 +30,82 @@ const MetafieldController  =  {
                         },
                     }
                 }).catch((e)=>console.log(e));
-                console.log("rs", JSON.stringify(rs.body.errors));
+                
+                resolve(rs);
+            });
+        };
+
+        let height = ()=>{
+            return new Promise(async (resolve, reject)=>{
+                let rs = await Graphql.query({
+                    data : {
+                        "query" : `mutation CreateMetafieldDefinition($definition: MetafieldDefinitionInput!) {
+                            metafieldDefinitionCreate(definition: $definition) {
+                                createdDefinition {
+                                    id
+                                    name
+                                }
+                                userErrors {
+                                    field
+                                    message
+                                    code
+                                }
+                            }
+                        }`,
+                        "variables": {
+                            "definition": {
+                              "name": "alto",
+                              "namespace": "dimensiones",
+                              "pin" : true,
+                              "key": "alto",
+                              "description": "defina el alto de su producto",
+                              "type": "number_integer",
+                              "ownerType": "PRODUCT"
+                            }
+                        },
+                    }
+                }).catch((e)=>console.log(e));
+                
+                resolve(rs);
+            });
+        };
+
+        let large = ()=>{
+            return new Promise(async (resolve, reject)=>{
+                let rs = await Graphql.query({
+                    data : {
+                        "query" : `mutation CreateMetafieldDefinition($definition: MetafieldDefinitionInput!) {
+                            metafieldDefinitionCreate(definition: $definition) {
+                                createdDefinition {
+                                    id
+                                    name
+                                }
+                                userErrors {
+                                    field
+                                    message
+                                    code
+                                }
+                            }
+                        }`,
+                        "variables": {
+                            "definition": {
+                              "name": "largo",
+                              "namespace": "dimensiones",
+                              "pin" : true,
+                              "key": "largo",
+                              "description": "defina el largo de su producto",
+                              "type": "number_integer",
+                              "ownerType": "PRODUCT"
+                            }
+                        },
+                    }
+                }).catch((e)=>console.log(e));
                 
                 resolve(rs);
             });
         };
       
-        return Promise.all([width()]);
+        return Promise.all([width(), height(), large()]);
     }
  }
 
