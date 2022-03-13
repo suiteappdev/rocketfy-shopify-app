68e768d07f399acbe5190b524b0939cf0f3e7db0
andrew <listerine1989@gmail.com>
2022-03-13 12:37:59 -0500

    log access token

--- controllers/MetafieldsController.js
+++ controllers/MetafieldsController.js
@@ -1,21 +1,40 @@
 import { DataType } from "@shopify/shopify-api";
 
 const MetafieldController  =  {
-    createDimensionMetafields : (client)=>{
-       
+    createDimensionMetafields : (RestClient, Graphql)=>{
         let createWidth = new Promise(async (resolve, reject)=>{
-            const data = await client.post({
+            const data = await RestClient.post({
                 path: 'metafields',
-                data: {"metafield":{"namespace":"inventory","validations": [], "visibleToStorefrontApi" : false, "pin": true, "key":"warehouse","value":25,"type":"number_integer"}},
+                data: {"metafield":{"owner" : "PRODUCT", "ownerType" : "PRODUCT", "namespace":"inventory","validations": [], "visibleToStorefrontApi" : false, "pin": true, "key":"warehouse","value":25,"type":"number_integer"}},
                 type: DataType.JSON,
             }).catch((e)=>reject(e));
+
+            /*await Graphql.query({ data: `
+                mutation ($input: MetafieldStorefrontVisibilityInput!) {
+                    metafieldStorefrontVisibilityCreate(input: $input) {
+                        metafieldStorefrontVisibility {
+                            id
+                        }
+                        userErrors {
+                            field
+                            message
+                        }
+                    }
+                }
+            `}, { variables : {
+                input: {
+                    "namespace": "custom_fields",
+                    "key": "suits",
+                    "ownerType": "PRODUCT"
+                }
+            }});*/
 
             console.log("Ancho", data);
             
             resolve(data);
         });
 
-        let createheight = new Promise( async (resolve, reject)=>{
+        /*let createheight = new Promise( async (resolve, reject)=>{
             const data = await client.post({
                 path: 'metafields',
                 data: {"metafield":{"ownerType" : "PRODUCT","namespace":"dimensiones","key":"Alto","value":0,"type":"number_integer"}},
@@ -33,7 +52,7 @@ const MetafieldController  =  {
             }).catch((e)=>reject(e));
 
             resolve(data);
-        });
+        });*/
 
         return Promise.all([createWidth]);
     }
