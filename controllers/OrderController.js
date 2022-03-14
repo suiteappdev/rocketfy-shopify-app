53ab4f5cec03caf0078bb3e2608429702a764eee
andrew <listerine1989@gmail.com>
2022-03-14 06:41:16 -0500

    edit graphql query

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -97,60 +97,12 @@ const OrderController  = {
                             data : {
                                 "query" : `query AppliedMetafieldDefinitionsForEdit($resourceId: ID!, $limit: Int!) {
                                     node(id: $resourceId) {
-                                      ... on HasMetafieldDefinitions {
-                                        appliedMetafieldDefinitions(
-                                          first: $limit
-                                          pinnedStatus: PINNED
-                                          sortKey: PINNED_POSITION
-                                        ) {
-                                          edges {
-                                            node {
-                                              ...AppliedMetafieldDefinitionFragment
-                                              __typename
-                                            }
-                                            __typename
-                                          }
-                                          __typename
-                                        }
-                                        __typename
-                                      }
-                                      __typename
+                                        id
+                                        namespace
+                                        value
                                     }
                                   }
-                                  
-                                  fragment AppliedMetafieldDefinitionFragment on AppliedMetafieldDefinition {
-                                    metafield {
-                                      id
-                                      value
-                                      __typename
-                                    }
-                                    definition {
-                                      id
-                                      ...MetafieldDefinitionFragment
-                                      __typename
-                                    }
-                                    __typename
-                                  }
-                                  
-                                  fragment MetafieldDefinitionFragment on MetafieldDefinition {
-                                    id
-                                    namespace
-                                    key
-                                    name
-                                    description
-                                    type {
-                                      name
-                                      __typename
-                                    }
-                                    pinnedPosition
-                                    validations {
-                                      name
-                                      type
-                                      value
-                                      __typename
-                                    }
-                                    __typename
-                                  }
+
                                 `,
                                 "variables": {
                                     "resourceId": `gid://shopify/Product/${line.product_id}`,
